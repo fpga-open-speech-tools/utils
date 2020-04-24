efb8eac27e92a126d9bf49f97681bcb226783323
Trevor Vannoy <trevorvannoy@montana.edu>
2020-04-24 11:41:53 -0600

    documentation and formatting

--- drivermgr.sh
+++ drivermgr.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
+
+# drivermgr.sh
+#
+# Load and remove kernel modules.
+#
+# Usage: drivermgr.sh load <directory>
+#        drivermgr.sh remove <directory>
+# 
+# This script assumes that the kernel modules we want to load
+# live in /lib/modules/<directory>. By convention, <directory>
+# matches the name of the device tree overlay that the kernel
+# modules are associated with. 
+#
+# author Trevor Vannoy
+# copyright 2020 Flat Earth Inc
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# Trevor Vannoy
+# Flat Earth Inc
+# 985 Technology Blvd
+# Bozeman, MT 59718
+# support@flatearthinc.com
+
 BASE_MODULES_PATH=/lib/modules
 VERBOSE=0
 
+# Load kernel modules
+#
+# This loads all modules in the user-provided directory
 function load() {
     for module in $MODULES_PATH/*.ko; do
         if [ $VERBOSE -eq 1 ]; then
@@ -11,6 +44,9 @@ function load() {
     done
 }
 
+# Remove kernel modules
+#
+# This removes all modules in the user-provided directory
 function remove() {
     for module in $MODULES_PATH/*.ko; do
         if [ $VERBOSE -eq 1 ]; then
@@ -20,10 +56,32 @@ function remove() {
     done
 }
 
+# Print verbose usage help
 function usage() {
-    echo "Usage: drivermgr load|remove directory [-h|--help] [-v|--verbose]"
+    echo "usage: drivermgr [-h] [-v] <command> directory"
+    echo
+    echo "Load and remove kernel modules."
+    echo 
+    echo "commands:"
+    echo "  load            load kernel module(s)"
+    echo "  remove          remove kernel module(s)"
+    echo
+    echo "positional arguments:"
+    echo "  directory       directory name in /lib/modules/ that contains the modules to be loaded"
+    echo 
+    echo "optional arguments:"
+    echo "  -h, --help      display this help text"
+    echo "  -v, --verbose   print more details about what's going on"
 }
 
+# Print brief usage help
+#
+# Primarily for when user's invoke the script incorrectly.
+function short_usage() {
+    echo "usage: drivermgr [-h] [-v] <command> directory"
+}
+
+# argument parsing
 for arg in "$@"; do
     case $arg in
         -h | --help)
@@ -37,7 +95,8 @@ for arg in "$@"; do
         ;;
         --*)
         echo "Unrecognized argument: $arg" 1>&2
-        usage
+        echo
+        short_usage
         exit 1
         ;;
         *)
@@ -49,14 +108,19 @@ for arg in "$@"; do
     esac
 done
 
-# make sure the user supplied 2 arguments
-if [ ${#POSITIONAL_ARGS[*]} -ne 2 ]; then
-    echo "Incorrect number of positional arguments." 1>&2
+if [ ${#POSITIONAL_ARGS[*]} -eq 0 ]; then
+    # print help when no arguments are supplied
     usage
+    exit 1
+elif [ ${#POSITIONAL_ARGS[*]} -ne 2 ]; then
+    # make sure the user supplied 2 arguments
+    echo "Incorrect number of positional arguments." 1>&2
+    echo
+    short_usage
     exit 1
 fi
 
-# grab command and overlay name from the positional arguments
+# grab command and modules directory name from the positional arguments
 COMMAND=${POSITIONAL_ARGS[0]}
 MODULES_PATH=$BASE_MODULES_PATH/${POSITIONAL_ARGS[1]}
 
@@ -70,7 +134,8 @@ case $COMMAND in
     ;;
     *)
     echo "Incorrect command. Must be either \"load\" or \"remove\"." 1>&2
-    usage
+    echo
+    short_usage
     exit 1
     ;;
 esac
--- overlaymgr.sh
+++ overlaymgr.sh
@@ -1,37 +1,115 @@
 #!/bin/bash
+
+# overlaymgr.sh
+#
+# Load and remove device tree overlays.
+#
+# Usage: overlaymgr.sh load <overlay_name>
+#        overlaymgr.sh remove <overlay_name>
+# 
+# This script uses the configfs overlay interface to load and 
+# remove device tree overlays at runtime. The overlays can
+# program the FPGA via the fpga manager, as well as add/remove
+# nodes in the device tree. In general, our overlays will do
+# both of the preceeding, but that need not always be the case.
+#
+# The overlay (.dtbo) and the FPGA bitstream (.rbf) need to be
+# located in /lib/firmware before running this script. It is assumed
+# that the overlay and bitstream have the same name, and are named according
+# to the Quartus project they came from.  
+#
+# See documentation/devicetree/configfs-overlays.txt in the kernel documentation
+# for more information on the configfs overlay interface.
+#
+# author Trevor Vannoy
+# copyright 2020 Flat Earth Inc
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# Trevor Vannoy
+# Flat Earth Inc
+# 985 Technology Blvd
+# Bozeman, MT 59718
+# support@flatearthinc.com
+
 CONFIGFS_PATH=/sys/kernel/config/device-tree/overlays
-FIRMWARE_PATH=/lib/firmware
 VERBOSE=0
 
+# Load the device tree overlay
+#
+# To load an overlay, we first need to create a directory in the configfs 
+# pseudofilesystem: /sys/kernel/config/device-tree/overlays/<overlay_name>
+# The kernel mounts configfs at /sys/kernel/config, and creates the 
+# device-tree/overlays directory for us. We echo the overlay file name to the
+# path property file that gets created for us in the <overlay_name> directory.
 function load() {
     if [ $VERBOSE -eq 1 ]; then
         echo "Making device tree overlay directory"
         echo "    mkdir $CONFIGFS_PATH/$OVERLAY_NAME"
         echo
-    fi;
+    fi
+    # make the overlay directory in configfs
     mkdir $CONFIGFS_PATH/$OVERLAY_NAME
 
     if [ $VERBOSE -eq 1 ]; then
         echo "Applying overlay"
         echo "    echo $OVERLAY_NAME.dtbo > $CONFIGFS_PATH/$OVERLAY_NAME/path"
         echo
-    fi;
+    fi
+    # echo the overlay file name to the path property file
     (echo $OVERLAY_NAME.dtbo > $CONFIGFS_PATH/$OVERLAY_NAME/path)
 }
 
+# Remove the device tree overlay.
+#
+# Overlays are removed by removing the configfs overlay directory we created:
+# /sys/kernel/config/device-tree/overlays/<overlay_name>. This removes nodes
+# from the device tree, but it doesn't unload the FPGA bitstream. When used to 
+# program the FPGA, overlays must be removed before loading in a new overlay 
+# (according to empirical testing, anyway). 
 function remove() {
     if [ $VERBOSE -eq 1 ]; then
         echo "Removing overlay directory"
         echo "    rmdir $CONFIGFS_PATH/$OVERLAY_NAME"
         echo
-    fi;
+    fi
+    # remove the configfs overlay directory
     rmdir $CONFIGFS_PATH/$OVERLAY_NAME
 }
 
+# Print verbose usage help
 function usage() {
-    echo "Usage: overlaymgr load|remove overlay [-h|--help] [-v|--verbose]"
+    echo "usage: overlaymgr [-h] [-v] <command> overlay"
+    echo
+    echo "Load and remove device tree overlays."
+    echo 
+    echo "commands:"
+    echo "  load            load a device tree overlay"
+    echo "  remove          remove a device tree overlay"
+    echo
+    echo "positional arguments:"
+    echo "  overlay         the name of the overlay file to load/remove"
+    echo 
+    echo "optional arguments:"
+    echo "  -h, --help      display this help text"
+    echo "  -v, --verbose   print more details about what's going on"
 }
 
+# Print brief usage help
+#
+# Primarily for when user's invoke the script incorrectly.
+function short_usage() {
+    echo "usage: overlaymgr [-h] [-v] <command> overlay"
+}
+
+
+# argument parsing
 for arg in "$@"; do
     case $arg in
         -h | --help)
@@ -45,7 +123,8 @@ for arg in "$@"; do
         ;;
         --*)
         echo "Unrecognized argument: $arg" 1>&2
-        usage
+        echo
+        short_usage
         exit 1
         ;;
         *)
@@ -57,10 +136,15 @@ for arg in "$@"; do
     esac
 done
 
-# make sure the user supplied two positional arguments
-if [ ${#POSITIONAL_ARGS[*]} -ne 2 ]; then
-    echo "Incorrect number of positional arguments." 1>&2
+if [ ${#POSITIONAL_ARGS[*]} -eq 0 ]; then
+    # print help when no arguments are supplied
     usage
+    exit 1
+elif [ ${#POSITIONAL_ARGS[*]} -ne 2 ]; then
+    # make sure the user supplied two positional arguments
+    echo "Incorrect number of positional arguments." 1>&2
+    echo
+    short_usage
     exit 1
 fi
 
@@ -78,7 +162,8 @@ case $COMMAND in
     ;;
     *)
     echo "Incorrect command. Must be either \"load\" or \"remove\"." 1>&2
-    usage
+    echo
+    short_usage
     exit 1
     ;;
 esac
