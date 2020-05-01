d35cd0a3af43f0d5fb8b9c5e1a34a53c9c48fe74
Trevor Vannoy <trevorvannoy@montana.edu>
2020-05-01 13:51:15 -0600

    documentation and pep8 formatting

--- awsdownload.py
+++ awsdownload.py
@@ -1,50 +1,56 @@
 #!/usr/bin/python3
 """
-This is a basic script that will download the specified files from an S3 bucket
+Download files for an SoC FPGA project from AWS.
 
-Arguments
+This script downloads the bitstream, device tree overlay, and device
+drivers located in a user-supplied directory in a user-supplied S3 bucket.
+
+Parameters
 ---------
-file_list : str, list
-    List of files to download from the S3 bucket
+s3bucket : str
+    Name of the S3 bucket
 
-directory : str
-    Directory in which all the files are located
+s3directory : str
+    Name of the S3 directory  in `s3bucket` where the desired files are located
 
-bucket    : str
-    Name of the S3 bucket to download files from
+firmware_path : str
+    Path where the bitstream and device tree overlay will be downloaded to
 
-Example
--------
-In this example, the user only wants to download a single file called DE10Nano_System.sof.  This is accomplished by 
-calling the script and providing three keyword arguments; the list of files (--file_list or -f), the directory of the 
-files (--directory or -d) and the name of  the S3 bucket (--bucket or -b).  The file is located in the nih-demos bucket 
-in the directory DE10-Nano/Passthrough/, so the function call  is as follows:
+driver_path : str
+    Path prefix where the device drivers will be downloaded to; drivers are
+    placed in a subdirectory of this path
 
-python3 download_files.py -fl DE10Nano_System.sof -b nih-demos -d DE10-Nano/Passthrough/ 
-
-Upon successful completion, the script will return :
-
-All files downloaded
-
-If more than one file in the directory needs to be downloaded, simply add more filenames to the list.  
-(e.g. -fl DE10Nano_system.sof soc_system.dtb DE10Nano_system.rbf)
+verbose : bool
+    Print verbose output
 
 Notes
 -----
-In order to run this script, the Amazon CLI credentials must be setup on the system.  This can be done by installing 
-AWS CLI using 
+boto3 must be installed on the system in order to run this script.
 
-sudo apt-get install awscli
+By convention, S3 directories are all lowercase, with words separated by hyphens
+when doing so improves readability. For example, the directory for the 
+Audio Mini sound effects project is audiomini/sound-effects. Additionally,
+The bitstream and device tree overlays are named the same as the project, but
+with underscores instead of hyphens, e.g. sound_effects.rbf. 
 
-then following the configuration instructions after executing 
+The directory name can be given with or without a trailing slash.
 
-aws configure
+Putting downloaded files in non-default path locations will affect the shell 
+scripts that load the overlays and device drivers since they expect the files 
+to be in the default locations. Using non-default locations is primarily for
+debugging purposes. 
 
-boto3 must also be installed on the system in order to run this script.  This is accomplished by executing 
+Examples
+--------
+Download files for the Audio Mini sound effects project
+$ ./awsdownload.py -b nih-demos -d audiomini/sound-effects
 
-pip3 install boto3
+Download files for the Audio Mini passthrough project, and put them in
+non-default locations
+$ ./awsdownload.py -b nih-demos -d audiomini/passthrough --firmware_path ~/firmware
 
-
+Copyright
+---------
 Copyright 2020 Flat Earth Inc
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
@@ -52,13 +58,11 @@ WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEM
 COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
 OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
 Trevor Vannoy, Tyler Davis
 Flat Earth Inc
 985 Technology Blvd
 Bozeman, MT 59718
 support@flatearthinc.com
-
 """
 
 import boto3
@@ -67,9 +71,17 @@ import argparse
 import os
 from botocore.client import Config
 from botocore import UNSIGNED
-from time import time
+
 
 def parseargs():
+    """
+    Parse command-line arguments.
+
+    Returns
+    -------
+    args : Namespace
+        Object containing the parsed arguments
+    """
     # Create the argument parser
     parser = argparse.ArgumentParser(add_help=False)
 
@@ -77,17 +89,25 @@ def parseargs():
     required_args = parser.add_argument_group('required arguments')
 
     # Add arguments for the directory and the bucket name
-    required_args.add_argument('-d', '--directory', help="S3 directory to download files from", type=str, required=True)
-    required_args.add_argument('-b', '--bucket', help="S3 bucket name", type=str, required=True)
+    required_args.add_argument('-d', '--directory', type=str, required=True,
+        help="S3 directory to download files from")
+    required_args.add_argument('-b', '--bucket', type=str, required=True,   
+        help="S3 bucket name")
 
     # Create an explicit group for optional arguments so the required arguments print first
     optional_args = parser.add_argument_group('optional arguments')
 
     # Add optional arguments
-    optional_args.add_argument('-h', '--help', help="show this help message and exit", action='help')
-    optional_args.add_argument('-v', '--verbose', help="print verbose output", default=False, action='store_true')
-    optional_args.add_argument('--firmware-path', help="path where bitstreams and overlays get placed (default: /lib/firmware/)", type=str, default='/lib/firmware/')
-    optional_args.add_argument('--driver-path', help="path prefix where kernel modules folder gets created (default: /lib/modules/)", type=str, default='/lib/modules/')
+    optional_args.add_argument('-h', '--help', action='help', 
+        help="show this help message and exit")
+    optional_args.add_argument('-v', '--verbose', action='store_true',
+        help="print verbose output", default=False)
+    optional_args.add_argument('--firmware-path', type=str, 
+        help="path where bitstreams and overlays get placed \
+        (default: /lib/firmware/)", default='/lib/firmware/')
+    optional_args.add_argument('--driver-path', type=str 
+        help="path prefix where kernel modules folder gets created \
+        (default: /lib/modules/)", default='/lib/modules/')
 
     # Parse the arguments
     args = parser.parse_args()
@@ -101,17 +121,47 @@ def parseargs():
     return args
 
 
-def main(s3bucket, s3directory, firmware_path, driver_path, verbose):
+def main(s3bucket, s3directory, firmware_path='/lib/firmware/',
+         driver_path='/lib/modules/', verbose=False):
+    """
+    Download files for an SoC FPGA project from AWS. 
+
+    This script downloads the bitstream, device tree overlay, and device
+    drivers located in a user-supplied directory in a user-supplied S3 bucket.
+
+    Parameters
+    ----------
+    s3bucket : str
+        Name of the S3 bucket
+
+    s3directory : str
+        Name of the S3 directory  in `s3bucket` where the desired files are located
+
+    firmware_path : str
+        Path where the bitstream and device tree overlay will be downloaded to
+
+    driver_path : str
+        Path prefix where the device drivers will be downloaded to; drivers are
+        placed in a subdirectory of this path
+
+    verbose : bool
+        Print verbose output
+    """
     # Create an s3 client that doesn't need/use aws credentials
-    client = boto3.client('s3', region_name='us-west-2', config=Config(signature_version=UNSIGNED))
+    client = boto3.client('s3', region_name='us-west-2',
+                          config=Config(signature_version=UNSIGNED))
 
     # Get all of the s3 objects that are in the desired bucket and directory.
     # The "directory" isn't really a directory, but a prefix in the object keys,
     # e.g. Key: some/directory/<actual_file>
-    objects = client.list_objects_v2(Bucket=s3bucket, Prefix=s3directory)['Contents']
+    # list_objects_v2 returns a big dictionary about the objects; the Contents 
+    # key is what contains the actual directory contents
+    objects = client.list_objects_v2(
+        Bucket=s3bucket, Prefix=s3directory)['Contents']
 
     # Get the keys for the bitstream (.rbf) and device tree overlay (.dtbo)
-    firmware_keys = [obj['Key'] for obj in objects if '.rbf' in obj['Key'] or '.dtbo' in obj['Key']]
+    firmware_keys = [obj['Key'] for obj in objects 
+                     if '.rbf' in obj['Key'] or '.dtbo' in obj['Key']]
 
     # Get the keys for the device drivers (.ko files)
     # If no drivers are in the s3 "directory", this list will be empty
@@ -122,9 +172,9 @@ def main(s3bucket, s3directory, firmware_path, driver_path, verbose):
 
     # If the driver list isn't empty
     if driver_keys:
-        # Get the project name that these drivers belong to. By convention, 
-        # this should be the same as the firmware file names (without the 
-        # file extension). 
+        # Get the project name that these drivers belong to. By convention,
+        # this should be the same as the firmware file names (without the
+        # file extension).
         driver_group_name = firmware_filenames[0].split('.')[0]
 
         # Create a directory for the drivers if one doesn't already exist
@@ -134,25 +184,20 @@ def main(s3bucket, s3directory, firmware_path, driver_path, verbose):
         # Get the driver filenames
         driver_filenames = [key.split('/')[-1] for key in driver_keys]
 
-    start = time()
     # Download the firmware files
-    for key,filename in zip(firmware_keys, firmware_filenames):
+    for key, filename in zip(firmware_keys, firmware_filenames):
         client.download_file(s3bucket, key, firmware_path + filename)
-    
+
     # If the driver list isn't empty, download the drivers
     if driver_keys:
-        for key,filename in zip(driver_keys, driver_filenames):
-            client.download_file(s3bucket, key, driver_path + 
-                driver_group_name + '/' + filename)
-    end = time()
-    print(end - start)
+        for key, filename in zip(driver_keys, driver_filenames):
+            client.download_file(s3bucket, key, driver_path 
+                                 + driver_group_name + '/' + filename)
 
     print('All files downloaded')
-
-
 
 
 if __name__ == "__main__":
     args = parseargs()
     main(args.bucket, args.directory, args.firmware_path, args.driver_path,
-        args.verbose)
+         args.verbose)
