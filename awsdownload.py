fc3d7f89161925dfe2891d53cdf9f7e1d2fe8312
Trevor Vannoy <trevorvannoy@montana.edu>
2020-04-30 20:42:28 -0600

    more robust and flexible version
    - add arguments for firmware and driver paths
    - handle case where no kernel modules are in the bucket
    - improve variable naming

--- awsdownload.py
+++ awsdownload.py
@@ -61,48 +61,98 @@ support@flatearthinc.com
 
 """
 
-import boto3, sys, argparse
+import boto3
+import sys
+import argparse
 import os
 from botocore.client import Config
 from botocore import UNSIGNED
+from time import time
 
 def parseargs():
     # Create the argument parser
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(add_help=False)
 
     # Create a new group for the required arguments
-    req_parser = parser.add_argument_group('Required Arguments')
+    required_args = parser.add_argument_group('required arguments')
 
-    # Add arguments for the file name list, directory, and the bucket name
-    # req_parser.add_argument('--file_list','-fl', nargs='+',help="List of filenames to download from the specified directory",type=str,required=True)
-    req_parser.add_argument('--directory','-d',help="Directory to download files from",type=str,required=True)
-    req_parser.add_argument('--bucket','-b',help="S3 bucket name",type=str,required=True)
+    # Add arguments for the directory and the bucket name
+    required_args.add_argument('-d', '--directory', help="S3 directory to download files from", type=str, required=True)
+    required_args.add_argument('-b', '--bucket', help="S3 bucket name", type=str, required=True)
+
+    # Create an explicit group for optional arguments so the required arguments print first
+    optional_args = parser.add_argument_group('optional arguments')
+
+    # Add optional arguments
+    optional_args.add_argument('-h', '--help', help="show this help message and exit", action='help')
+    optional_args.add_argument('-v', '--verbose', help="print verbose output", default=False, action='store_true')
+    optional_args.add_argument('--firmware-path', help="path where bitstreams and overlays get placed (default: /lib/firmware/)", type=str, default='/lib/firmware/')
+    optional_args.add_argument('--driver-path', help="path prefix where kernel modules folder gets created (default: /lib/modules/)", type=str, default='/lib/modules/')
 
     # Parse the arguments
     args = parser.parse_args()
+
+    # Ensure firmware and driver paths end in a trailing slash
+    if args.firmware_path[-1] != '/':
+        args.firmware_path += '/'
+    if args.driver_path[-1] != '/':
+        args.driver_path += '/'
+
     return args
+
+
+def main(s3bucket, s3directory, firmware_path, driver_path, verbose):
+    # Create an s3 client that doesn't need/use aws credentials
+    client = boto3.client('s3', region_name='us-west-2', config=Config(signature_version=UNSIGNED))
+
+    # Get all of the s3 objects that are in the desired bucket and directory.
+    # The "directory" isn't really a directory, but a prefix in the object keys,
+    # e.g. Key: some/directory/<actual_file>
+    objects = client.list_objects_v2(Bucket=s3bucket, Prefix=s3directory)['Contents']
+
+    # Get the keys for the bitstream (.rbf) and device tree overlay (.dtbo)
+    firmware_keys = [obj['Key'] for obj in objects if '.rbf' in obj['Key'] or '.dtbo' in obj['Key']]
+
+    # Get the keys for the device drivers (.ko files)
+    # If no drivers are in the s3 "directory", this list will be empty
+    driver_keys = [obj['Key'] for obj in objects if '.ko' in obj['Key']]
+
+    # Get the firmware filenames (the part of the key after the last slash)
+    firmware_filenames = [key.split('/')[-1] for key in firmware_keys]
+
+    # If the driver list isn't empty
+    if driver_keys:
+        # Get the project name that these drivers belong to. By convention, 
+        # this should be the same as the firmware file names (without the 
+        # file extension). 
+        driver_group_name = firmware_filenames[0].split('.')[0]
+
+        # Create a directory for the drivers if one doesn't already exist
+        if not os.path.isdir(driver_path + driver_group_name):
+            os.mkdir(driver_path + driver_group_name)
+
+        # Get the driver filenames
+        driver_filenames = [key.split('/')[-1] for key in driver_keys]
+
+    start = time()
+    # Download the firmware files
+    for key,filename in zip(firmware_keys, firmware_filenames):
+        client.download_file(s3bucket, key, firmware_path + filename)
+    
+    # If the driver list isn't empty, download the drivers
+    if driver_keys:
+        for key,filename in zip(driver_keys, driver_filenames):
+            client.download_file(s3bucket, key, driver_path + 
+                driver_group_name + '/' + filename)
+    end = time()
+    print(end - start)
+
+    print('All files downloaded')
+
 
 
 
 if __name__ == "__main__":
     args = parseargs()
-
-    # Set the boto resource
-    client = boto3.client('s3', region_name='us-west-2', config=Config(signature_version=UNSIGNED))
-
-    objects = client.list_objects_v2(Bucket=args.bucket, Prefix=args.directory)['Contents']
-
-    firmware_files = [obj['Key'] for obj in objects if '.rbf' in obj['Key'] or '.dtbo' in obj['Key']]
-    driver_files = [obj['Key'] for obj in objects if '.ko' in obj['Key']]
-
-    # TODO: driver files might be empty because the overlay may not need drivers, so we will need to handle this case...
-    driver_dir = driver_files[0].split('/')[-2].replace('-','_')
-    if not os.path.isdir('lib/modules/' + driver_dir):
-        os.mkdir('lib/modules/' + driver_dir)
-
-    for f in firmware_files:
-        client.download_file(args.bucket, f, 'lib/firmware/' + f.split('/')[-1])
-    for f in driver_files:
-        client.download_file(args.bucket, f, 'lib/modules/' + driver_dir + '/' + f.split('/')[-1])
-
-    print('All files downloaded')
+    main(args.bucket, args.directory, args.firmware_path, args.driver_path,
+        args.verbose)
