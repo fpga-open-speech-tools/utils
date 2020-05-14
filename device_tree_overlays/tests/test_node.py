195fe2007d1bb0f3fb8de6351dac44d053762bd2
Trevor Vannoy <trevorvannoy@montana.edu>
2020-05-14 15:43:37 -0600

    add more test cases

--- device_tree_overlays/tests/test_node.py
+++ device_tree_overlays/tests/test_node.py
@@ -16,13 +16,64 @@ class TestNode(unittest.TestCase):
         self.assertEqual(expected, str(n))
 
     def test_one_child_node(self):
-        self.fail()
+        expected = (
+            '&i2c {\n'
+            '   compatible = "dev,i2c";\n'
+            '   adc0: adc {\n'
+            '       compatible = "dev,adc";\n'
+            '   };\n'
+            '};'
+        )
+
+        i2c = Node(name='i2c', compatible='dev,i2c')
+        adc0 = Node(name='adc', label='adc0', compatible='dev,adc')
+
+        i2c.children = adc0
+
+        self.assertEqual(expected, str(i2c))
 
     def test_two_child_nodes(self):
-        self.fail()
+        expected = (
+            '&i2c {\n'
+            '    compatible = "dev,i2c";\n'
+            '    adc0: adc {\n'
+            '        compatible = "dev,adc";\n'
+            '    };\n'
+            '    eeprom0: eeprom {\n'
+            '        compatible = "dev,eeprom";\n'
+            '    };\n'
+            '};'
+        )
+
+        i2c = Node(name='i2c', compatible='dev,i2c')
+        adc0 = Node(name='adc', label='adc0', compatible='dev,adc')
+        eeprom0 = Node(name='eeprom', label='eeprom0', compatible='dev,eeprom')
+
+        i2c.children = [adc0, eeprom0]
+
+        self.assertEqual(expected, str(i2c))
 
     def test_nested_child_nodes(self):
-        self.fail()
+        expected = (
+            '&parent {\n'
+            '    compatible = "parent";\n'
+            '    child0: child {\n'
+            '        compatible = "child";\n'
+            '        child1: child {\n'
+            '            compatible = "child";\n'
+            '        };\n'
+            '    };\n'
+            '};'
+        )
+
+        parent = Node(name='parent', compatible='parent')
+        child0 = Node(name='child', label='child0', compatible='child')
+        child1 = Node(name='child', label='child1', compatible='child')
+
+        parent.children = [child0]
+        child0.children = [child1]
+
+        self.assertEqual(expected, str(parent))
 
 
 if __name__ == "__main__":
