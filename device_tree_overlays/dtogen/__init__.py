64d6c15882772614da9e225a7d0e7d996f0a89b8
Trevor Vannoy <vannoy@flatearthinc.com>
2020-06-15 13:54:55 -0600

    update authors and copyright

--- device_tree_overlays/dtogen/__init__.py
+++ device_tree_overlays/dtogen/__init__.py
@@ -1,5 +1,4 @@
 __title__ = 'dtogen'
-__author__ = 'Trevor Vannoy <vannoy@flatearthinc.com>'
+__author__ = 'Trevor Vannoy <vannoy.trevor@gmail.com>, Dylan Wickham <wickham@flatearthinc.com>'
 __license__ = 'GPL v3'
-__copyright__ = 'Copyright 2020 Flat Earth Inc.'
-__all__ = ('DeviceTreeNode', 'FpgaRegionNode')
+__copyright__ = 'Copyright 2020 Audio Logic'
