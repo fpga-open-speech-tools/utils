f150f8f8779dd7146ae4a04f2cfe0135a503abd5
Trevor Vannoy <trevorvannoy@montana.edu>
2020-09-01 14:54:19 -0600

    remove dtogen since it now lives in simulink_codegen

--- device_tree_overlays/dtogen/__init__.py
+++
@@ -1,4 +0,0 @@
-__title__ = 'dtogen'
-__author__ = 'Trevor Vannoy <vannoy.trevor@gmail.com>, Dylan Wickham <wickham@flatearthinc.com>'
-__license__ = 'GPL v3'
-__copyright__ = 'Copyright 2020 Audio Logic'
