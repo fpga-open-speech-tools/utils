63898c0e90a01324cc52446d22a6311d2fbbb873
Connor Dack <dack@flatearthinc.com>
2020-10-26 15:49:45 -0600

    Seperated Bash Script into multiple services
    Created Service for Web App and Deployment Manager
    Updated the Packaging to Copy, Install, and Uninstall new services

--- frost_edge/packaging/scripts/frost_edge_lkms.sh
+++ frost_edge/packaging/scripts/frost_edge_lkms.sh
@@ -4,11 +4,3 @@ insmod /lib/modules/frost/FE_AD1939.ko;
 insmod /lib/modules/frost/FE_AD7768_4.ko;	
 insmod /lib/modules/frost/FE_PGA2505.ko;
 insmod /lib/modules/frost/FE_TPA613A2.ko;
-
-echo "Starting Frost Edge Web App.";
-cd /var/lib/frost/web_app/publish/;
-./OpenSpeechTools &
-
-echo "Starting Deployment Manager."
-cd /var/lib/frost/deployment_manager/;
-node EndpointServer.js
