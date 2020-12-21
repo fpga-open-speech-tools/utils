02d2ff752f8bd6447941f201ef211a192d2eb20c
Connor Dack <dack@flatearthinc.com>
2020-12-21 09:49:35 -0700

    Removed commented out line

--- rootfs/docker_build.sh
+++ rootfs/docker_build.sh
@@ -1,6 +1,5 @@
 docker build -t build_rootfs .
 docker run --name build_rootfs --privileged build_rootfs 
-#docker run --name build_rootfs --privileged --entrypoint /bin/bash -it build_rootfs 
 docker cp build_rootfs:/tmp/rootfs.tar.gz .
 docker rm -f build_rootfs
 docker rmi -f build_rootfs
