f1e960323e484917c5b129dace6cddb680d07cf2
Connor Dack <dack@flatearthinc.com>
2021-01-06 09:23:57 -0700

    Initial Commit

---
+++ make_sdcard/audiomini/docker_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+# Build the FrOST Root FS with Docker
+docker build -t build_rootfs .
+docker run --name build_rootfs --privileged build_rootfs 
+docker cp build_rootfs:/tmp/rootfs.tar.gz .
+docker rm -f build_rootfs
+docker rmi -f build_rootfs
