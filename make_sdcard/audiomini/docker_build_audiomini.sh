d2c971bfe45492dbc805f5cf67ee31882860046b
Connor Dack <dack@flatearthinc.com>
2021-01-06 11:24:44 -0700

    Working on Debugging the Docker Build

--- make_sdcard/audiomini/docker_build_audiomini.sh
+++ make_sdcard/audiomini/docker_build_audiomini.sh
@@ -5,7 +5,8 @@ cp ../frost_usd_card_blueprint.xml ./DistroBlueprint.xml
 project="build_audiomini_image"
 
 docker build -t $project .
-docker run --name $project --privileged $project 
+# docker run --name $project --privileged $project 
+docker run --entrypoint "/bin/bash" -it --name $project --cap-add=CAP_MKNOD --privileged $project 
 docker cp $project:/tmp/audio-mini.img .
 docker rm -f $project
 docker rmi -f $project
