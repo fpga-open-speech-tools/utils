2d3554793b26d1093036aa1a9455ba32be844452
Connor Dack <dack@flatearthinc.com>
2021-01-06 14:16:47 -0700

    Updated the final file

--- rootfs/docker_build.sh
+++ rootfs/docker_build.sh
@@ -3,6 +3,6 @@
 # Build the FrOST Root FS with Docker
 docker build -t build_rootfs .
 docker run --name build_rootfs --privileged build_rootfs 
-docker cp build_rootfs:/tmp/rootfs.tar.gz .
+docker cp build_rootfs:/tmp/frost_rootfs.tar.gz .
 docker rm -f build_rootfs
 docker rmi -f build_rootfs
