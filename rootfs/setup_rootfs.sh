768e647b4b279b72a68729f32218b73708f43470
Connor Dack <53094583+dack-fe@users.noreply.github.com>
2021-01-28 10:54:23 -0700

    Updated the default password (#27)
    * Updated the default password
    
    * Removed the to-do comment

--- rootfs/setup_rootfs.sh
+++ rootfs/setup_rootfs.sh
@@ -51,11 +51,9 @@ curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin
 echo
 echo "setting up user account..."
 echo
+
 # setup our root user
-# TODO: we really should not be using root for everything
-#       we should create a normal user and disable the root account, 
-#       or at least give it a strong password...
-echo "root:root" | chpasswd
+echo "root:frost1234" | chpasswd
 
 # we're done; exit the chroot
 echo "done setting up rootfs..."
